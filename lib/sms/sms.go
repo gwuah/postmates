26943a5d734342f8bf6bdd63cd8d8d979c5805c2
gwuah <griffithawuah15@gmail.com>
2020-09-20 22:58:48 +0000

    added sms messaging layer

---
+++ lib/sms/sms.go
@@ -0,0 +1,70 @@
+package sms
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type SMS struct {
+	key string
+}
+
+type Message struct {
+	To      string `json:"to"`
+	From    string `json:"from"`
+	Sms     string `json:"sms"`
+	Type    string `json:"type"`
+	Channel string `json:"channel"`
+	ApiKey  string `json:"api_key"`
+}
+
+type Response struct {
+	MessageId string `json:"message_id"`
+	Message   string `json:"message"`
+	Balance   int    `json:"balance"`
+	User      string `json:"user"`
+}
+
+const API_ENDPOINT = "https://termii.com/api/sms/send"
+
+func New(apiKey string) *SMS {
+
+	return &SMS{apiKey}
+}
+
+func (w *SMS) SendTextMessage(msg Message) (*Response, error) {
+	body, err := json.Marshal(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", API_ENDPOINT, bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	payload, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var response Response
+
+	err = json.Unmarshal(payload, &response)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
